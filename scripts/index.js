d7a0442d6c61172502c021b0624fa03a0e1dcdb1
DONYIELL <Donyiellco@gmail.com>
2025-05-08 03:12:07 -0500

    update JSscript

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,11 @@
-const editProfileBtn = document.querySelector("edit-profile-btn");
-const editProfileModal = document.querySelector("edit-profile-modal");
-const editProfileCloseBtn = editProfileModal.querySelector("modal__close-btn");
+const profileEditbtn = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+
+profileEditbtn.addEventListener("click", function () {
+  editProfileModal.classList.add("modal__is-opened");
+});
+
+modalCloseBtn.addEventListener("click", function () {
+  editProfileCloseBtn.classList.add("modal__close-btn");
+});
