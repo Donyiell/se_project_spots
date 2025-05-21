d35875ae0076444144308687632161bc9dab1865
DONYIELL <Donyiellco@gmail.com>
2025-05-21 04:37:20 -0500

    js update

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,18 @@
-const profileEditbtn = document.querySelector(".profile__edit-btn");
+const profileEditBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
-const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+
+const profileAddBtn = document.querySelector("profile__add-btn");
+const profileAddModal = document.querySelector("#profile__add-btn");
+//const profileAddCloseBtn
 
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__discription");
 
-profileEditbtn.addEventListener("click", function () {
-  editProfileNameInput.value = profileDescriptionEl.textContent;
+profileEditBtn.addEventListener("click", function () {
+  //editProfileName.value = profileNameEl.textContent;
+  //editProfileDescription.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal__is-opened");
 });
 
@@ -15,4 +20,18 @@ editProfileCloseBtn.addEventListener("click", function () {
   editProfileModal.classList.remove("modal__is-opened");
 });
 
+profileAddBtn.addEventListener("click", function () {
+  editProfileModal.classList.add("modal__is-opened");
+});
 
+newPostCloseBtn.addEventListener("click", function () {
+  newPostModal.classList.remove("modal_is-opened");
+  ij;
+});
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  console.log("submitting");
+}
+
+editProfileForm.addEventListener("submit");
