e43b83f397dc7ca71027bece5c77a4cd56459cc7
DONYIELL <Donyiellco@gmail.com>
2025-05-22 01:45:09 -0500

    js

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const profileEditBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 const editProfileForm = editProfileModal.querySelector(".modal__form");
-const editProfileNameInput = editProfileModal.querySelector(".profile-name-input");
+const editProfileNameInput = editProfileModal.querySelector("#profile-name-input");
 const editProfileDescriptionInput = editProfileModal.querySelector("#profile-name-input");
 // - select based on id
 
@@ -14,7 +14,7 @@ const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
 
 profileEditBtn.addEventListener("click", function () {
-  //editProfileNameInput.value = profileNameEl.textContent;
+  // editProfileNameInput.value = profileNameEl.textContent;
   //editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal__is-opened");
 });
@@ -27,15 +27,21 @@ profileAddBtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal__is-opened");
 });
 
-newPostBtn.addEventListener("click", function () {
-  ProfileAddModal.classList.remove("modal_is-opened");
-  ij;
-});
+
+//  TODO declare newPostBtn
+// newPostBtn.addEventListener("click", function () {
+//   ProfileAddModal.classList.remove("modal_is-opened");
+//   ij;
+// });
+
+// TODO Create event listeners for the new post form
 
 function handleEditProfileSubmit(evt) {
+  console.log('Event ', evt)
   evt.preventDefault();
   profileNameEl.textContent = editProfileNameInput.value;
   //TODO - Handler the other
+  // Add text content for the description field - Civl Aviation
 }
 
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
